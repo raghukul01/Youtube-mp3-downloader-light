c43bd0920782f6a30442c846ba3fd88e7b78ce4f
Raghukul Raman <raghukul.raman01@gmail.com>
2019-04-04 20:39:03 +0530

    Add download script

--- download.sh
+++ download.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    youtube-dl -o '%(title)s.%(ext)s' -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --no-check-certificate $line
+done < "$1"
 
